eb0a0037039a3351fa9e36543d30ca72ed5a294e
subbumoni <subramanianbeee@gmail.com>
2024-07-29 12:28:45 +0530

    Applican basic express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const http_server = express()
+
+const PORT = 5000
+const HOSTNAME = 'localhost'
+
+http_server.listen(PORT, HOSTNAME, () => {
+    console.log(`http://${HOSTNAME}:${PORT}`)
+})
+
+
+http_server.get("/", (req, res) => {
+    res.status(200).json({
+        message:"Server started successfully",
+    })
+})
